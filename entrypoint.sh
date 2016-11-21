8a08d50338da2954b12a05c929808d1ecc62ca56
weiliang <496928838@qq.com>
2016-11-22 00:23:12 +0800

    2016年11月22日00:23:09

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,8 @@
 #! /bin/sh
-echo "-------------author: ${AUTHOR}---------------"
-
-echo $AUTHOR > author.txt
-echo $URL > URL.txt
-
-
 
 wget $URL -O default.conf > wget.txt
 
 mv default.conf /etc/nginx/conf.d/default.conf > mv.txt
-
-
 
 
 nginx -g "daemon off;"
